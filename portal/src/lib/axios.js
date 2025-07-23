fedcf5a5f5f8c0458ed1a05a1f1d00c8562cd1c3
Husein Hashi <huseinarahman6@gmail.com>
2025-07-23 11:14:34 +0300

    23 july update

--- portal/src/lib/axios.js
+++ portal/src/lib/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3200/api/v1",
+  baseURL: "https://eyeclinicset.up.railway.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
